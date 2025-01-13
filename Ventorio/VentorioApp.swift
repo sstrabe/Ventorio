4427d4ef5d5b7ffd1fac997e568c7a10eb256094
sstrabe <sstrabe09@gmail.com>
2025-01-13 22:51:41 +0100

    Initial Commit

---
+++ Ventorio/VentorioApp.swift
@@ -0,0 +1,17 @@
+//
+//  VentorioApp.swift
+//  Ventorio
+//
+//  Created by Samuel Street on 13.01.25.
+//
+
+import SwiftUI
+
+@main
+struct VentorioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VentorioTests/VentorioTests.swift
@@ -0,0 +1,17 @@
+//
+//  VentorioTests.swift
+//  VentorioTests
+//
+//  Created by Samuel Street on 13.01.25.
+//
+
+import Testing
+@testable import Ventorio
+
+struct VentorioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
